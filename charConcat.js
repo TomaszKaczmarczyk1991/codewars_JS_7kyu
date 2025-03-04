41555b2b5c340a94122b98a2e2b5ed77b9debdeb
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-03-04 12:26:34 +0100

    add charConcat.js

---
+++ charConcat.js
@@ -0,0 +1,17 @@
+function charConcat(string) {
+  let str = [...string]; 
+  let result = [];
+  let counter = 0;
+
+
+  if (str.length % 2 !== 0) {
+    str.splice(Math.floor(str.length / 2), 1);
+  }
+
+  for (let i = 0; i < str.length / 2; i++) {
+    counter++;
+    result.push(str[i], str[str.length - 1 - i], counter);
+  }
+
+  return result.join("");
+}
