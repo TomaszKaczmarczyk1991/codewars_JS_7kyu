64ba68b9b4962cf5e636bbd68700be9da1363a48
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-11 14:39:31 +0100

    add transpose.js

---
+++ transpose.js
@@ -0,0 +1,6 @@
+function transpose(song, interval){
+    const standard = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+
+  }
+
+console.log(transpose(['A'])) // It should output: ['A#']
