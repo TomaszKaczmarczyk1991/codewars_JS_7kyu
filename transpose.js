d57ef89c077340805b6320726dcfc7705f5ed33a
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-11 14:41:43 +0100

    add first step

--- transpose.js
+++ transpose.js
@@ -1,6 +1,6 @@
 function transpose(song, interval){
     const standard = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
-
+    return standard[0 + interval]
   }
 
-console.log(transpose(['A'])) // It should output: ['A#']
+console.log(transpose(['A'], 1)) // It should output: ['A#']
