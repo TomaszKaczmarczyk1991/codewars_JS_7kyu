e1b468128076d7fbb568bfbf4dcf6460b74a3437
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-11 15:22:14 +0100

    add better solution

--- transpose.js
+++ transpose.js
@@ -21,4 +21,9 @@ function transpose(song, interval){
     return transposed;
 }
 
-console.log(transpose(['A', 'C', 'G#'], -1))
+// Someone's better solution:
+
+// function transpose(song, interval){
+//     const notes = [['A'], ['A#', 'Bb'], ['B', 'Cb'] , ['C', 'B#'], ['C#', 'Db'], ['D'], ['D#', 'Eb'], ['E', 'Fb'], ['F', 'E#'], ['F#', 'Gb'], ['G'], ['G#','Ab']];
+//     return song.map(a => notes[(notes.length + notes.findIndex(b => b.includes(a)) + interval) % notes.length][0]);
+//   }
