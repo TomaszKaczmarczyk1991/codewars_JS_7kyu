72b34f8533fbfae1ed35a794d506a61d21e624f0
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-30 00:02:00 +0100

    add hiddenWord.js

---
+++ hiddenWord.js
@@ -0,0 +1,22 @@
+function hiddenWord(num) {
+    const numStr = num.toString();
+    const key = {
+        6 : 'a',
+        1 : 'b',
+        7 : 'd',
+        4 : 'e',
+        3 : 'i',
+        2 : 'l',
+        9 : 'm',
+        8 : 'n',
+        0 : 'o',
+        5 : 't'
+    }
+    
+    const result = [];
+
+    for(let i = 0; i < numStr.length; i++){
+        if(key.hasOwnProperty(numStr[i])) result.push(key[Number(numStr[i])]);
+    }
+   return result.join('');
+}
