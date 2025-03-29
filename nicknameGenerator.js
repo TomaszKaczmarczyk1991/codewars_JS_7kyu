e93ab5ff3a8b490b072d6efb06aab8c81638a2d5
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-29 04:12:28 +0100

    add solution that passed all the tests

--- nicknameGenerator.js
+++ nicknameGenerator.js
@@ -2,9 +2,7 @@ function nicknameGenerator(name){
     const vowels = ['a', 'e', 'i', 'o', 'u'];
 
     if(name.length < 4) return "Error: Name too short";
-    return (vowels.includes(name[2])) ? [...name].toSpliced(4).join('') : [...name].toSpliced(3).join('');
+    return (vowels.includes(name[2])) ? [name[0], name[1], name[2], name[3]].join('') : [name[0], name[1], name[2]].join('');
   }
 
-console.log(nicknameGenerator('Gregory')) // Greg
-
-// Rozwiazanie nie przechodzi testow na Codewars. Metoda toSpliced() została wprowadzona w Node.js w wersji 20. Obecnie Codewars korzysta z wersji Node 18, która nie obsługuje tej metody
+console.log(nicknameGenerator('Robert')) // Rob
