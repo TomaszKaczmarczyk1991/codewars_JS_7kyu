e2f0374805a961675f569b0af43c1eeff3dff136
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-31 00:10:46 +0200

    optimize score calculation using Set for faster lookup

--- maxPossibleScore.js
+++ maxPossibleScore.js
@@ -1,13 +1,13 @@
 function maxPossibleScore(obj, arr) {
     let result = 0;
+    const bonusQuestions = new Set(arr);
 
-    for(const q in obj) {
-        // console.log(obj[q])
-        if(arr.includes(q)) result += obj[q] * 2;
-        else result += obj[q];
+    for (const q of Object.keys(obj)) {
+        result += obj[q] * (bonusQuestions.has(q) ? 2 : 1);
     }
+    
     return result;
-  }
+}
 
 
 console.log(maxPossibleScore({"a": 1, "b": 2, "c": 3}, ["a", "c"])); // 1 * 2 + 2 + 3 * 2 = 10
