3b5d68274d3e9a93431c16968c0a26c19039ccb6
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-04-01 05:37:29 +0200

    add scoreboard.js

--- scoreboard.js
+++ scoreboard.js
@@ -21,10 +21,3 @@ console.log(scoreboard([
     {name: "Habanero Hillary", chickenwings: 5 , hamburgers: 17, hotdogs: 11},
     {name: "Big Bob" , chickenwings: 20, hamburgers: 4, hotdogs: 11}
   ]));
-
-
-  // TARGET OUTPUT:
-//   [
-//     {name: "Big Bob", score: 134},
-//     {name: "Habanero Hillary", score: 98}
-//   ]
