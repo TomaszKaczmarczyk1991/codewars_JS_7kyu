ce9bc83372697cdbc46dc4268ca6be0631183bb7
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-04-03 06:02:41 +0200

    refactor sXORe function to avoid timeouts for large inputs

--- sxore.js
+++ sxore.js
@@ -1,11 +1,8 @@
 function sxore(n) {
-    let result = 0;
-
-    for (let i = 0; i <= n; i++) {
-        result ^= i;
-    }
-    
-    return result;
+    if (n % 4 === 0) return n;
+    if (n % 4 === 1) return 1;
+    if (n % 4 === 2) return n + 1;
+    return 0;
 }
 
   console.log(sxore(50)) // 51
