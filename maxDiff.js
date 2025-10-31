04493128a0c09f9c53ad8b5c356a658c0f74c518
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-10-31 22:04:04 +0100

    handle empty array

--- maxDiff.js
+++ maxDiff.js
@@ -1,6 +1,6 @@
 function maxDiff(list) {
-    const diff = Math.max(...list) - Math.min(...list);
-    return diff !== -Infinity ? diff : 0;
-};
+  if (list.length < 2) return 0;
+  return Math.max(...list) - Math.min(...list);
+}
 
 console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6]))
