8b744224cb8ae456a64f303d19578ec99e355a62
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-12 03:45:24 +0100

    sumTwoSmallestNumbers.js

---
+++ sumTwoSmallestNumbers.js
@@ -0,0 +1,6 @@
+function sumTwoSmallestNumbers(numbers) {
+    const sorted = [...numbers].sort((a, b) => a - b);
+    return sorted[0] + sorted[1];
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
