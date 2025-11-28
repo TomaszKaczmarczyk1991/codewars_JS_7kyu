e6596b6c7121675cbf316a5372d1c2238b43932f
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-28 03:34:21 +0100

    add sequenceSum.js

---
+++ sequenceSum.js
@@ -0,0 +1,14 @@
+const sequenceSum = (begin, end, step) => {
+  if(begin > end) return 0
+  let result = 0
+  let current = begin
+
+  while(current <= end){
+    if(current > end) continue
+    result += current
+    current += step
+  }
+  return result
+}
+
+console.log(sequenceSum(1,5,3))
