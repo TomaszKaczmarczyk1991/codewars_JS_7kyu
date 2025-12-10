a1bc3cfc2be30cde24c182c3a57abbfe7eb71771
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-12-10 02:08:38 +0100

    fix: replace my stupid, incorrect charCodeAt argument with 0

--- printerError.js
+++ printerError.js
@@ -2,7 +2,7 @@ function printerError(s) {
   const arr = [...s]
   
   let counter = arr.reduce((acc, val) => {
-    if (val.charCodeAt(val) > 109) acc += 1
+    if (val.charCodeAt(0) > 109) acc += 1
     return acc
   }, 0)
 
