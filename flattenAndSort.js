9bd6893a2c044a1f3b992c343ebf7f62822bf373
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-12-22 19:38:20 +0100

    add flattenAndSort.js

---
+++ flattenAndSort.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function flattenAndSort(array) {
+  return array.flat().sort((a, b) => a - b);
+}
+
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]])) // [1, 2, 3, 4, 5, 6, 7, 8, 9]
