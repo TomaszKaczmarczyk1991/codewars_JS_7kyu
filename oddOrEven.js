805153e214feb9d3619fabb944b191140bfb389f
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2026-02-15 23:13:35 +0100

    add better solution

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,13 @@
-function oddOrEven(array) {
-   if(array.length === 0) return 'even'
+// function oddOrEven(array) {
+//    if(array.length === 0) return 'even'
 
-   const sum = array.reduce((acc, val) => {
-    return acc += val
-   },0)
+//    const sum = array.reduce((acc, val) => {
+//     return acc += val
+//    },0)
 
-   return (sum % 2 === 0) ? 'even' : 'odd'
+//    return (sum % 2 === 0) ? 'even' : 'odd'
+// }
+
+function oddOrEven(arr) {
+  return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
 }
