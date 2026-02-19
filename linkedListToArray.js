c5e9c85c317f4ac146afc6ef32b1655c95315feb
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2026-02-19 21:52:22 +0100

    add linkedListToArray.js

---
+++ linkedListToArray.js
@@ -0,0 +1,12 @@
+function listToArray(list) {
+    const arr = []
+    let current = list
+    
+    while(current){
+        arr.push(current.value)
+        current = current.next
+    }
+    return arr
+}
+
+console.log(listToArray({value: 1, next: {value: 2, next: {value: 3, next: null}}}))
