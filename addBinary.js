b0771b28bfb88bb72e3bda8ecf8a5851f4d3a855
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2026-03-11 02:41:50 +0100

    simplify addBinary function

--- addBinary.js
+++ addBinary.js
@@ -1,13 +1,3 @@
 function addBinary(a,b) {
-  let sum = +a + +b
-  const binary = []
-  
-  if(sum === 0) return "0"
-  
-  while(sum > 0){
-    binary.push(sum % 2)
-    sum = Math.floor(sum / 2)
-  }
-  
-  return binary.reverse().join('')
+    return (a+b).toString(2)
 }
