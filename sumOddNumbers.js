98ffef0015947380afca23b7c62c278c8f1e7432
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2026-04-21 14:51:10 +0200

    simplify

--- sumOddNumbers.js
+++ sumOddNumbers.js
@@ -1,11 +1,3 @@
 function rowSumOddNumbers(n) {
-  const nRowNums = []
-  let num = n * (n - 1) + 1
-  let result = 0
- 
-  for(let i = 0; i < n; i++){
-    result += num
-    num += 2
-  }
-  return result
+  return n**3
 }
