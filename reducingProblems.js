bc0aca3fa31597c6aba9b84bd4e91c0378155a6d
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2026-08-31 00:03:38 +0200

    add reducingProblems.js

---
+++ reducingProblems.js
@@ -0,0 +1,7 @@
+function calculateTotal(team1, team2) {
+  let t1s = team1.reduce((t, c) => t + c, 0);
+  let t2s = team2.reduce((t, c) => t + c, 0);
+  return t1s > t2s;
+}
+
+console.log(calculateTotal([57,2,1],[]))
