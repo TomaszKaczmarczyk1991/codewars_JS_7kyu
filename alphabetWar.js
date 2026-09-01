73e0cfb544eb2fb033a1267563a684792b47447e
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2026-09-02 01:27:13 +0200

    add alphabetWar.js

---
+++ alphabetWar.js
@@ -0,0 +1,20 @@
+function alphabetWar(fight){
+   const leftSide = {w: 4, p: 3, b: 2, s: 1}
+   const rightSide = {m: 4, q: 3, d: 2, z: 1}
+   
+   let leftResult = 0
+   let rightResult = 0
+   
+   for(let i of fight){
+       if(leftSide[i]) leftResult += leftSide[i]
+   }
+   
+   for(let i of fight){
+       if(rightSide[i]) rightResult += rightSide[i]
+   }
+   
+   return leftResult === rightResult ? "Let's fight again!" : leftResult > rightResult ?
+   "Left side wins!" : "Right side wins!"
+}
+
+console.log(alphabetWar("zzzzs")) // Right side wins!
