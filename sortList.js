4fbbebcad5de41661aa9c2602e3ca1764321acfa
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2026-09-08 17:02:34 +0200

    simplify sortList.js

--- sortList.js
+++ sortList.js
@@ -1,5 +1,3 @@
 function sortList(sortBy, list) { 
-    const arr = [...list]
-    if(arr.length === 0) return []
-    return arr.sort((a,b) => b[sortBy] - a[sortBy])
+    return [...list].sort((a,b) => b[sortBy] - a[sortBy])
 }
